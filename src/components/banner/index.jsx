d174909b50e00ee3e175f3e4ebfdc283e75b6c96
Marcelo <marcelorcarnauba@gmail.com>
2023-08-15 11:56:02 -0300

    aula-01-commit

---
+++ src/components/banner/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Banner () {
+    return (
+        <div>
+            <img src="./src/assets/HeaderHeader_total.png"/>
+        </div>
+    )
+}
+
+export default Banner;
